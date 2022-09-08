746389dd7b4f7745b3c645209a240ae17250a857
AntonPlish <antonplish@gmail.com>
2022-09-08 10:57:49 +0300

    Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,14 @@ import ContactList from './Contacts/ContactList';
 import Filter from './Filter/Filter';
 
 export default function App() {
-  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? [
-    { id: 'id-1', name: 'Rosie Simpson', number: '+38044-459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '+38055-443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '+38066-645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '+38077-227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '+38044-459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '+38055-443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '+38066-645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '+38077-227-91-26' },
+    ]
+  });
 
   const [filter, setFilter] = useState('');
 
